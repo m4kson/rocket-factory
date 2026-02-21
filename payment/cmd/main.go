ac358a66404ec991516f3f64dd2d4685dda1a9a5
Maksim Rudenko <limos.00@mail.ru>
2026-02-21 13:17:40 +0300

    hw1-payment-service-logic-implemented

--- payment/cmd/main.go
+++ payment/cmd/main.go
@@ -1,1 +1,62 @@
 package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/google/uuid"
+	paymentV1 "github.com/m4kson/rocket-factory/shared/pkg/proto/payment/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+)
+
+const grpcPort = "50051"
+
+type paymentService struct {
+	paymentV1.UnimplementedPaymentServiceServer
+}
+
+func (s *paymentService) PayOrder(_ context.Context, request *paymentV1.PayOrderRequest) (*paymentV1.PayOrderResponse, error) {
+	transactionId := uuid.New().String()
+	fmt.Println("transactionId:", transactionId)
+
+	return &paymentV1.PayOrderResponse{
+		TransactionUuid: transactionId,
+	}, nil
+}
+
+func main() {
+	lis, err := net.Listen("tcp", ":"+grpcPort)
+	if err != nil {
+		log.Printf("failed to listen: %v", err)
+		return
+	}
+
+	s := grpc.NewServer()
+
+	service := &paymentService{}
+
+	paymentV1.RegisterPaymentServiceServer(s, service)
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("gRPC server is listening on port %s\n", grpcPort)
+		if err := s.Serve(lis); err != nil {
+			log.Printf("failed to serve: %v\n", err)
+			return
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down gRPC server...")
+	s.GracefulStop()
+	log.Println("Server gracefully stopped")
+}
