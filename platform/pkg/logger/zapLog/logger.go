7473ab28843009de7b63cb7297e5ec455bcd8172
Maksim Rudenko <limos.00@mail.ru>
2026-04-26 16:16:24 +0300

    hw4 - zap added

---
+++ platform/pkg/logger/zapLog/logger.go
@@ -0,0 +1,193 @@
+package zapLog
+
+import (
+	"context"
+	"os"
+	"strings"
+	"sync"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type Key string
+
+const (
+	traceIDKey Key = "trace_id"
+	userIDKey  Key = "user_id"
+)
+
+var (
+	globalLogger *logger
+	initOnce     sync.Once
+	dynamicLevel zap.AtomicLevel
+)
+
+type logger struct {
+	zapLogger *zap.Logger
+}
+
+func Init(levelStr string, asJSON bool) error {
+	initOnce.Do(func() {
+		dynamicLevel = zap.NewAtomicLevelAt(parseLevel(levelStr))
+		encoderCfg := buildProductionEncoderConfig()
+
+		var encoder zapcore.Encoder
+		if asJSON {
+			encoder = zapcore.NewJSONEncoder(encoderCfg)
+		} else {
+			encoder = zapcore.NewConsoleEncoder(encoderCfg)
+		}
+
+		core := zapcore.NewCore(
+			encoder,
+			zapcore.AddSync(os.Stdout),
+			dynamicLevel,
+		)
+
+		zapLogger := zap.New(core, zap.AddCaller(), zap.AddCallerSkip(2))
+
+		globalLogger = &logger{
+			zapLogger: zapLogger,
+		}
+	})
+
+	return nil
+}
+
+func parseLevel(levelStr string) zapcore.Level {
+	switch strings.ToLower(levelStr) {
+	case "debug":
+		return zapcore.DebugLevel
+	case "info":
+		return zapcore.InfoLevel
+	case "warn", "warning":
+		return zapcore.WarnLevel
+	case "error":
+		return zapcore.ErrorLevel
+	default:
+		return zapcore.InfoLevel
+	}
+}
+
+func buildProductionEncoderConfig() zapcore.EncoderConfig {
+	return zapcore.EncoderConfig{
+		TimeKey:        "timestamp",                 // время
+		LevelKey:       "level",                     // уровень логирования
+		NameKey:        "logger",                    // имя логгера, если используется
+		CallerKey:      "caller",                    // откуда вызван лог
+		MessageKey:     "message",                   // текст сообщения
+		StacktraceKey:  "stacktrace",                // стектрейс для ошибок
+		LineEnding:     zapcore.DefaultLineEnding,   // перенос строки
+		EncodeLevel:    zapcore.CapitalLevelEncoder, // INFO, ERROR
+		EncodeTime:     zapcore.ISO8601TimeEncoder,  // читаемый ISO 8601 формат
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder, // короткий caller
+		EncodeName:     zapcore.FullNameEncoder,
+	}
+}
+
+func SetLevel(levelStr string) {
+	if dynamicLevel == (zap.AtomicLevel{}) {
+		return
+	}
+
+	dynamicLevel.SetLevel(parseLevel(levelStr))
+}
+
+func Logger() *logger {
+	return globalLogger
+}
+
+func SetNopLogger() {
+	globalLogger = &logger{
+		zapLogger: zap.NewNop(),
+	}
+}
+
+func Sync() error {
+	if globalLogger != nil {
+		return globalLogger.zapLogger.Sync()
+	}
+
+	return nil
+}
+
+func With(fields ...zap.Field) *logger {
+	if globalLogger == nil {
+		return &logger{zapLogger: zap.NewNop()}
+	}
+
+	return &logger{
+		zapLogger: globalLogger.zapLogger.With(fields...),
+	}
+}
+
+func WithContext(ctx context.Context) *logger {
+	if globalLogger == nil {
+		return &logger{zapLogger: zap.NewNop()}
+	}
+
+	return &logger{
+		zapLogger: globalLogger.zapLogger.With(fieldsFromContext(ctx)...),
+	}
+}
+
+func Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	globalLogger.Debug(ctx, msg, fields...)
+}
+
+func Info(ctx context.Context, msg string, fields ...zap.Field) {
+	globalLogger.Info(ctx, msg, fields...)
+}
+
+func Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	globalLogger.Warn(ctx, msg, fields...)
+}
+
+func Error(ctx context.Context, msg string, fields ...zap.Field) {
+	globalLogger.Error(ctx, msg, fields...)
+}
+
+func Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	globalLogger.Fatal(ctx, msg, fields...)
+}
+
+func (l *logger) Debug(ctx context.Context, msg string, fields ...zap.Field) {
+	allFields := append(fieldsFromContext(ctx), fields...)
+	l.zapLogger.Debug(msg, allFields...)
+}
+
+func (l *logger) Info(ctx context.Context, msg string, fields ...zap.Field) {
+	allFields := append(fieldsFromContext(ctx), fields...)
+	l.zapLogger.Info(msg, allFields...)
+}
+
+func (l *logger) Warn(ctx context.Context, msg string, fields ...zap.Field) {
+	allFields := append(fieldsFromContext(ctx), fields...)
+	l.zapLogger.Warn(msg, allFields...)
+}
+
+func (l *logger) Error(ctx context.Context, msg string, fields ...zap.Field) {
+	allFields := append(fieldsFromContext(ctx), fields...)
+	l.zapLogger.Error(msg, allFields...)
+}
+
+func (l *logger) Fatal(ctx context.Context, msg string, fields ...zap.Field) {
+	allFields := append(fieldsFromContext(ctx), fields...)
+	l.zapLogger.Fatal(msg, allFields...)
+}
+
+func fieldsFromContext(ctx context.Context) []zap.Field {
+	fields := make([]zap.Field, 0)
+
+	if traceID, ok := ctx.Value(traceIDKey).(string); ok && traceID != "" {
+		fields = append(fields, zap.String(string(traceIDKey), traceID))
+	}
+
+	if userID, ok := ctx.Value(userIDKey).(string); ok && userID != "" {
+		fields = append(fields, zap.String(string(userIDKey), userID))
+	}
+
+	return fields
+}
